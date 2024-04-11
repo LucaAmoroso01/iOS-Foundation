a5d37ada3eed8fb888232640b02957f60f1d7ae1
roccodelprete <122788299+roccodelprete@users.noreply.github.com>
2024-04-11 10:10:13 +0200

    create project EcoPlay

---
+++ EcoPlay/EcoPlay/EcoPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoPlayApp.swift
+//  EcoPlay
+//
+//  Created by Rocco Del Prete on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
